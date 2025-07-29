c5ba115eebf89cab1a8e37694a5daa9a6897e37f
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-07-29 10:06:57 -0300

    feat(Show_lobinho.js): função que busca lobos e função que exclui
    Ainda não atualiza na memória para facilitar testes.

---
+++ Lobinhos/Show_Lobinho/show_lobinho.js
@@ -0,0 +1,53 @@
+// script pra acessar o lobinho.json
+async function inicializarLocalStorage() {
+    try {
+        const response = await fetch('../../lobinhos.json');
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar lobinho.json: ${response.statusText}`);
+        }
+        const lobos = await response.json();
+        localStorage.setItem('lobos', JSON.stringify(lobos));
+        console.log('Lobos inicializados no localStorage');
+    } catch (error) {
+        console.error('Erro ao inicializar o localStorage:', error);
+    } finally {
+        console.log('Tentativa de inicialização do localStorage concluída');
+    }
+}
+
+if (!localStorage.getItem('lobos')) {
+    inicializarLocalStorage().then(() => {
+        console.log('Inicialização do localStorage concluída');
+    }).catch(error => {
+        console.error('Erro durante a inicialização do localStorage:', error);
+    });
+}
+
+let lobos = JSON.parse(localStorage.getItem('lobos'));
+
+// função que exclui um lobinho da lista segundo sua posição no array de objetos e atualiza a memória
+function excluir(posicao) { // a posição pode ser obtida através da função Busca: ex.: excluir(busca(10))
+    lobos.splice(posicao, 1)
+    //!!! Para atualizar a memória: (ainda em forma de comentário para não atualizá-la durante os debugs) !!!
+    //localStorage.setItem('lobos', JSON.stringify(lobo
+}
+
+// função que busca o lobinho pelo seu ID -- retorna a posição dele no array de objetos
+// pode ser alterado para qualquer parametro: é so mudar o que está sendo comparado
+function busca(id) {
+    meio = Math.floor(lobos.length)
+    if (id >= (lobos[lobos.length/2].id)){
+        for(let i = (lobos.length/2); i < lobos.length; i++){
+            if (id == lobos[i].id){
+                return i
+            }
+        }
+    }
+    else {
+        for(let i = 0; i <= meio; i++){
+            if (id == lobos[i].id){
+                return i
+            }
+        }
+    }
+}
