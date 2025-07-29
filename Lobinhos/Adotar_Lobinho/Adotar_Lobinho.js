75114b441962db2aa42519d2186c054e025a9710
Joao Vitor Moutella Lopes <Jvmoutella@gmail.com>
2025-07-29 02:01:06 -0300

    Finalizacao do Adotar Lobinho

---
+++ Lobinhos/Adotar_Lobinho/Adotar_Lobinho.js
@@ -0,0 +1,48 @@
+async function inicializarLocalStorage() {
+    try {
+        const response = await fetch('../../lobinhos.json');
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar lobinho.json: ${response.statusText}`);
+        }
+        const lobos = await response.json();
+        localStorage.setItem('lobos', JSON.stringify(lobos));
+        console.log('Lobos inicializados no localStorage');
+    } catch (error) {
+        console.error('Erro ao inicializar o localStorage:', error);
+    } finally {
+        console.log('Tentativa de inicialização do localStorage concluída');
+    }
+}
+
+if (!localStorage.getItem('lobos')) {
+    inicializarLocalStorage().then(() => {
+        console.log('Inicialização do localStorage concluída');
+    }).catch(error => {
+        console.error('Erro durante a inicialização do localStorage:', error);
+    });
+}
+
+let lobos = JSON.parse(localStorage.getItem('lobos'));
+
+let loboSelecionado = lobos[0];
+
+document.getElementById("Imagem").src = loboSelecionado.imagem
+document.getElementById("nome").textContent = loboSelecionado.nome;
+document.getElementById("ID").textContent = loboSelecionado.id;
+
+let nome = document.querySelector("#inputNome");
+let idade = document.querySelector("#inputIdade");
+let email = document.querySelector("#inputEmail");
+let btnAdotar = document.querySelector(".Adotar");
+
+function adicionarDados() {
+
+    let nomeDono = nome.value;
+    let idadeDono = idade.value;
+    let emailDono = email.value;
+    let objeto = {id: loboSelecionado.id, nome: loboSelecionado.nome, idade: loboSelecionado.idade, descricao: loboSelecionado.descricao, imagem: loboSelecionado.imagem, adotado: true, nomeDono: nomeDono, idadeDono: idadeDono, emailDono: emailDono}
+    lobos[ID] = objeto
+    localStorage.setItem('lobos', JSON.stringify(lobos));
+}
+
+btnAdotar.addEventListener("click", adicionarDados);
