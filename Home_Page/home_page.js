6935f7fa002b116f594d179d82ac83c7bb6a9874
Gabriel Baeta <gabrielbaeta@id.uff.br>
2025-07-29 18:55:18 -0300

    add: imagem direcionar para show lobinho

--- Home_Page/home_page.js
+++ Home_Page/home_page.js
@@ -1,22 +1,26 @@
 let lobos = JSON.parse(localStorage.getItem('lobos'));
 
 // função que altera o texto interno aleatoriamente
-function altera(id_nome, id_idade, id_descricao, id_imagem) {
-    id_css = [id_nome, id_idade, id_descricao, id_imagem]
+function altera(id_nome, id_idade, id_descricao, id_imagem,  id_link) {
+    id_css = [id_nome, id_idade, id_descricao, id_imagem,  id_link]
     //variaveis do ids alterados
     let nome_alterado = document.querySelector(id_css[0])
     let idade_alterado = document.querySelector(id_css[1])
     let descricao_alterado = document.querySelector(id_css[2])
     let img_alterado = document.querySelector(id_css[3])
+    let link_alterado= document.querySelector(id_css[4])
 
     //numero aleatório entre 0 e o tamanho do array de objetos (corrigido o bug do +1)
     let num_aletorio = Math.floor(Math.random() * lobos.length)
 
     //troca dos ids por estatísticas de um lobo aleatório
+
     nome_alterado.innerText = lobos[num_aletorio].nome
     idade_alterado.innerText = lobos[num_aletorio].idade
     descricao_alterado.innerText = lobos[num_aletorio].descricao
     img_alterado.src = lobos[num_aletorio].imagem
+
+    link_alterado.href = '../Lobinhos/Show_Lobinho/Show_Lobinho.html?id=' + lobos[num_aletorio].id;
 
     // Atualiza o botão baseado no status de adoção
     const botaoContainer = nome_alterado.closest('.informacoes').querySelector('.lobinho-botao-container')
@@ -49,8 +53,8 @@ function altera(id_nome, id_idade, id_descricao, id_imagem) {
     }
 }
 
-altera("#subtitulo1", "#idade1", "#descricao1", "#imagem1")
-altera("#subtitulo2", "#idade2", "#descricao2", "#imagem2")
+altera("#subtitulo1", "#idade1", "#descricao1", "#imagem1", "#link-imagem1")
+altera("#subtitulo2", "#idade2", "#descricao2", "#imagem2", "#link-imagem2")
 
 // funcao pra ir pra pagina de adocao
 function irParaAdocao(idDoLobo) {
